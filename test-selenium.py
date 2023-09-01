48a799a0a088f783ae9b06ed1b7cbf8cc1aa7991
Stefano Peretti <49915584+stefanoppp@users.noreply.github.com>
2023-09-01 11:22:36 -0300

    Add files via upload

--- test-selenium.py
+++ test-selenium.py
@@ -3,28 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from time import sleep
 
 def main():
      # Accedemos a pagina ppal
-    libro="Ingenieria del software"
-    autor="Pressman"
+    libro="Ingenieria del software de Pressman"
+    
     try: 
         try:
             driver = webdriver.Chrome()
-            wait = WebDriverWait(driver, 20)
+            wait = WebDriverWait(driver, 5)
             driver.get("https://www.um.edu.ar/biblio/")
+            driver.maximize_window()
         except:
             print("La web de la facultad no responde")
 
-    # Pagina biblioteca
+    # Accedemos a web de la biblio
         try:
             element = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Ir al Catálogo")))
             driver.execute_script("arguments[0].click();", element)
         except:
             print("La web de la biblioteca no responde")
 
+    # Switcheamos la pestana y buscamos el libro
         try:
             new_window=driver.window_handles[1]
             driver.switch_to.window(new_window)
@@ -37,11 +37,6 @@ def main():
             confirm=driver.find_element(By.ID,"numresults")
             confirm = confirm.text
             print(confirm)
-            
-            book=driver.find_element(By.PARTIAL_LINK_TEXT, libro)
-            book = book.text
-            print(book)
-            # search_bar.send_keys(Keys.RETURN)
 
         except Exception as e:
             print(e)
